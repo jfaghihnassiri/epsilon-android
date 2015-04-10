3e9d773bcf193ce307a29878393cffa2d75e6b32
Jhon Faghih-Nassiri <jfaghihnassiri@gmail.com>
2015-04-10 00:31:59 -0700

    pushing first app with a simple camera view

---
+++ app/src/main/java/com/team/epsilon/CameraActivity.java
@@ -0,0 +1,29 @@
+package com.team.epsilon;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.FrameLayout;
+import android.hardware.Camera;
+
+
+public class CameraActivity extends ActionBarActivity {
+
+    private Camera mCamera;
+    private CameraPreview mPreview;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Create an instance of Camera
+        mCamera = CameraUtilities.getCameraInstance();
+
+        // Create our Preview view and set it as the content of our activity.
+        mPreview = new CameraPreview(this, mCamera);
+        FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
+        preview.addView(mPreview);
+    }
+}
---
+++ app/src/main/java/com/team/epsilon/CameraUtilities.java
@@ -0,0 +1,33 @@
+package com.team.epsilon;
+
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+
+/**
+ * Created by Nassiri on 4/10/2015.
+ */
+public class CameraUtilities {
+    /** A safe way to get an instance of the Camera object. */
+    public static Camera getCameraInstance(){
+        Camera c = null;
+        try {
+            c = Camera.open(); // attempt to get a Camera instance
+        }
+        catch (Exception e){
+            // Camera is not available (in use or does not exist)
+        }
+        return c; // returns null if camera is unavailable
+    }
+
+    /** Check if this device has a camera */
+    private boolean checkCameraHardware(Context context) {
+        if (context.getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA)){
+            // this device has a camera
+            return true;
+        } else {
+            // no camera on this device
+            return false;
+        }
+    }
+}
